de65ee30f604dd6bd74b442c80df1ea2f48c64ec
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-02-16 23:11:39 +0600

    add remove funtionality

--- js/script.js
+++ js/script.js
@@ -1,28 +1,24 @@
 document.getElementById('add-note').addEventListener('click', function () {
     const noteText = document.getElementById('note').value;
+    if (noteText.length < 20) {
+        alert('Please enter at least 20 characters');
+        return;
+    }
     const note = document.createElement('div');
     note.classList.add('flex', 'gap-6', 'justify-between', 'bg-white', 'rounded-md', 'p-4');
     note.innerHTML =
         `<p class="text-gray-700">${noteText}</p>
-        <button class="remove-icon">
-            <svg width="20" height="20" viewBox="0 0 20 20">
-                <path d="M17 2h-3.5l-1-1h-5l-1 1H3v2h14zM4 17a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V5H4z" />
-            </svg>
-        </button>`
+        <button class="remove-icon bi bi-trash-fill"></button>`
 
     document.getElementById('note-list').appendChild(note);
-    addListenerToRemoveIcons();
 });
 
 
-function addListenerToRemoveIcons(){
-    const removeIcons = document.getElementsByClassName('remove-icon');
-    for (const removeIcon of removeIcons) {
-        removeIcon.addEventListener('click', function () {
-            console.log('hi');
-        });
+document.getElementById('note-list').addEventListener('click', function(e){
+    console.log(e.target.parentNode);
+    if (e.target.classList.contains('remove-icon')) {
+        e.target.parentNode.remove(); // remove function does not have parameter
     }
-}
-addListenerToRemoveIcons();
+})
 
 
